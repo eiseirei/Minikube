85dbb290f85fb1fffabc658815b9e701d965f9f1
Seirei <eiseirei@gmail.com>
2023-09-24 18:13:19 +0300

    Update

---
+++ src/index.php
@@ -0,0 +1,33 @@
+﻿<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <title>Клиенты организации</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+  </head>
+  <body>
+    <h1>Клиенты организации</h1>
+    <table align="center">
+      <tr>
+        <th>Фамилия</th>
+        <th>Имя</th>
+        <th>Отчество</th>
+        <th>Дата рождения</th>
+      </tr>
+      <?php
+				$mysqli = new mysqli($_ENV["DB_URL"], $_ENV["username"], $_ENV["password"], $_ENV["database"]);
+				$result = $mysqli->query("SELECT Name, Surname, Patronym, DATE_FORMAT(BirthDate, \"%d.%m.%Y\") as BirthDate_rus  FROM customs");
+	foreach ($result as $row) {
+      		echo "
+      			<tr>
+        		<td>{$row['Surname']}</td>
+        		<td>{$row['Name']}</td>
+        		<td>{$row['Patronym']}</td>
+        		<td>{$row['BirthDate_rus']}</td>
+     			 </tr>
+      		"; } ?>
+    </table>
+    <?php
+			phpinfo();
+		?>
+  </body>
+</html>
